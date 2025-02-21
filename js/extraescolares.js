316b68bf802b3eec9a2f03cbe0a6ce523407672e
Jesus <jesusanesredondo@gmail.com>
2025-02-21 10:31:52 +0100

    Tabla

--- js/extraescolares.js
+++ js/extraescolares.js
@@ -4,3 +4,50 @@ btnDescargar.addEventListener('click', function () {
     link.download = "extraescolares-judo-dojo-tarraco.pdf";
     link.click();
 });
+
+
+// Obtener todas las celdas
+const campusJudo = document.querySelectorAll('.campusJudo');
+
+// Añadir el evento mouseenter a todas las celdas
+campusJudo.forEach(celda => {
+    celda.addEventListener('mouseenter', () => {
+        // Agregar la clase hover a todas las celdas excepto la actual
+        campusJudo.forEach(otraCelda => {
+            if (otraCelda !== celda) {
+                otraCelda.classList.add('hover-effect');
+            }
+        });
+    });
+
+    // Añadir el evento mouseleave para eliminar la clase hover
+    celda.addEventListener('mouseleave', () => {
+        // Eliminar la clase hover de todas las celdas
+        campusJudo.forEach(otraCelda => {
+            otraCelda.classList.remove('hover-effect');
+        });
+    });
+});
+
+// Obtener todas las celdas
+const campusPatinaje = document.querySelectorAll('.campusPatinaje');
+
+// Añadir el evento mouseenter a todas las celdas
+campusPatinaje.forEach(celda => {
+    celda.addEventListener('mouseenter', () => {
+        // Agregar la clase hover a todas las celdas excepto la actual
+        campusPatinaje.forEach(otraCelda => {
+            if (otraCelda !== celda) {
+                otraCelda.classList.add('hover-effect');
+            }
+        });
+    });
+
+    // Añadir el evento mouseleave para eliminar la clase hover
+    celda.addEventListener('mouseleave', () => {
+        // Eliminar la clase hover de todas las celdas
+        campusPatinaje.forEach(otraCelda => {
+            otraCelda.classList.remove('hover-effect');
+        });
+    });
+});
