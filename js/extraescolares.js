ee39c64e0da07a1baaf6af00450dd470d8fb8d17
Jesus <jesusanesredondo@gmail.com>
2025-02-21 14:15:08 +0100

    version 1

--- js/extraescolares.js
+++ js/extraescolares.js
@@ -51,3 +51,8 @@ campusPatinaje.forEach(celda => {
         });
     });
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
